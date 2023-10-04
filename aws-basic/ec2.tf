6c8eb5e1ec86fd842394e143f6e7ba157f595d17
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-04 12:22:33 -0400

    Update ec2.tf

--- aws-basic/ec2.tf
+++ aws-basic/ec2.tf
@@ -11,7 +11,7 @@ provider "aws" {
     region = "us-east-2"
  }
 
-resource "aws_instance" "mmw.t2.micro" {
+resource "aws_instance" "mmw-t2-micro" {
     ami = "ami-026b57f3c383c2eec"
     instance_type = "t2.micro"
 }
