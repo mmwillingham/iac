8e0d80bb7bce5b16e6300ab37955fc8022f066a0
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-04 12:17:19 -0400

    initial terraform

---
+++ aws-basic/ec2.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "-> 4.6"
+    }
+  }
+}
+ provider "aws" {
+    region = "us-east-2"
+ }
+
+resource "aws_instance" "mmw.t2.micro" {
+    ami = "ami-026b57f3c383c2eec"
+    instance_type = "ts.micro"
+}
