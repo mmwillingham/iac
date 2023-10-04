8834d315c1fec7c4026fb3925b38ab179697cf7b
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-04 12:20:47 -0400

    Update ec2.tf

--- aws-basic/ec2.tf
+++ aws-basic/ec2.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "-> 4.6"
+        version = "~> 4.6"
     }
   }
 }
@@ -12,5 +12,5 @@ terraform {
 
 resource "aws_instance" "mmw.t2.micro" {
     ami = "ami-026b57f3c383c2eec"
-    instance_type = "ts.micro"
+    instance_type = "t2.micro"
 }
