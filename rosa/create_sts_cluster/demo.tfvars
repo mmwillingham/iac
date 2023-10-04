9c92f0ec348fc339cb66e158175886c4ffe02ff8
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-04 17:25:12 -0400

    rafa

---
+++ rosa/create_sts_cluster/demo.tfvars
@@ -0,0 +1,16 @@
+ocm_environment = "local"
+account_role_prefix = "demo"
+url = "https://api.openshift.com"
+
+#availability_zones = "us-east-2a"
+cloud_region = "us-east-2"
+cluster_name = "bolauder88"
+operator_role_prefix = "demo"
+# Optional
+admin_password = "cluster-admin"
+admin_username = "cluster-@dmin"
+# openshift_version = "4.13"
+# tags=<aws_resource_tags>
+# ec2_metadata_http_tokens=<required_or_optional>
+# path
+# tags
---
+++ rosa/create_sts_cluster/variables.tf
@@ -0,0 +1,56 @@
+variable "operator_role_prefix" {
+  type = string
+}
+
+variable "account_role_prefix" {
+  type = string
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-cluster"
+}
+
+variable "cloud_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-2a"]
+}
+
+variable "tags" {
+  description = "List of AWS resource tags to apply."
+  type        = map(string)
+  default     = null
+}
+
+variable "path" {
+  description = "(Optional) The arn path for the account/operator roles as well as their policies."
+  type        = string
+  default     = null
+}
+
+variable "aws_subnet_id" {
+  type    = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "ocm_environment" {
+  type    = string
+  default = "local"
+}
+
+variable "openshift_version" {
+  type = string
+  default = ""
+}
