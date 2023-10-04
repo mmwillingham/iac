eb58d7f384af0fd35bf7ec04eecda4df4915b118
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-04 13:05:19 -0400

    .

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # iac
-# Create basic AWS resources
+## Create an AWS accunt
+### demo.redhat.com > labs > AWS ILT
+
+## Create basic AWS resources
 ```bash
 cd aws-basic
 terraform init
@@ -14,6 +17,7 @@ terraform destroy -auto-approve
 ```
 
 # Next steps
+## add outputs
 ## terraform cloud - store state there
 ## kickoff terraform from something else
 ## variables
--- aws-basic/aws.tf
+++ aws-basic/aws.tf
@@ -8,13 +8,20 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
  }
 
 resource "aws_instance" "mmw-t2-micro" {
     ami = "ami-026b57f3c383c2eec"
     instance_type = "t2.micro"
+    tags = {
+      Name = "mmw-t2-micro"
+  }
 }
 
-resource "aws_vpc" "mmw" {
+resource "aws_vpc" "mmw_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "mmw_vpc"
+  }
 }
