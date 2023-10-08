9af90fdb1a6b50510041c27eff6777790a05c7d6
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-08 14:12:35 -0400

    .

--- gitops/main.tf
+++ gitops/main.tf
@@ -10,36 +10,9 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-#resource "kubernetes_namespace" "example" {
-#  metadata {
-#    annotations = {
-#      name = "example-annotation"
-#    }
-
-#    labels = {
-#      mylabel = "label-value"
-#    }
-#
-#    name = var.namespace
-#  }
-#}
-
-resource "kubernetes_pod" "nginx" {
+resource "kubernetes_namespace" "example" {
   metadata {
-    name = "nginx-example"
-#    labels {
-#      App = "nginx"
-#    }
-  }
-
-  spec {
-    container {
-      image = "nginx:1.15.2"
-      name  = "example"
-
-      port {
-        container_port = 80
-      }
-    }
+    name = var.namespace
   }
 }
+
