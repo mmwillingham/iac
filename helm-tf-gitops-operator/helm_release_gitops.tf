ee3a95bd2c2949a72317c4ccb60c238228a1c32a
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-08 17:56:57 -0400

    .

--- helm-tf-gitops-operator/helm_release_gitops.tf
+++ helm-tf-gitops-operator/helm_release_gitops.tf
@@ -2,7 +2,7 @@ resource "helm_release" "openshift-gitops" {
   name        = "openshift-gitops"
   chart       = "openshift-gitops"
   repository  = "./charts"
-  namespace   = "helm-openshift-gitops "
+  namespace   = "helm-openshift-gitops"
   max_history = 3
   create_namespace = true
   wait             = true
