582f4e280852ce99b193cfba4b7243f7afd8409d
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-08 14:17:13 -0400

    .

--- gitops/main-helm.tf
+++ gitops/main-helm.tf
@@ -1,5 +1,13 @@
+#provider "helm" {
+#  kubernetes {
+#    config_path = "~/.kube/config"
+#  }
+#}
+
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+  host = var.host
+  username = var.username
+  password = var.password    
   }
 }
