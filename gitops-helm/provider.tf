20d5c93ade8e9dc560305cd60a234f0187f4d1ba
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 16:05:25 -0400

    .

--- gitops-helm/provider.tf
+++ gitops-helm/provider.tf
@@ -1,12 +1,14 @@
-#provider "helm" {
-#    #config_path = "~/.kube/config"
-#    host = var.host
-#    username = var.username
-#    password = var.password
-#}
-
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    #config_path = "~/.kube/config"
+    host = var.host
+    username = var.username
+    password = var.password
   }
 }
+
+#provider "helm" {
+#  kubernetes {
+#    config_path = "~/.kube/config"
+#  }
+#}
