3b3d4b6f562678a32ba2a03880024761aebf8760
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 17:37:40 -0400

    .

--- gitops-helm/helm_release_gitops.tf
+++ gitops-helm/helm_release_gitops.tf
@@ -1,10 +1,15 @@
 resource "helm_release" "gitops" {
   name        = "gitops"
   chart       = "gitops"
-  repository  = "./charts"
-  namespace   = "helm-gitops"
-  max_history = 3
-  create_namespace = true
-  wait             = true
-  reset_values     = true
+#  repository  = "./charts"
+#  namespace   = "helm-gitops"
+#  max_history = 3
+#  create_namespace = true
+#  wait             = true
+#  reset_values     = true
 }
+
+#resource "helm_release" "example" {
+#  name       = "my-local-chart"
+#  chart      = "./charts/example"
+#}
