e350d7883b266b0f896e5a21ebcbc210af40be7d
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 16:49:54 -0400

    .

--- gitops-helm/variables.tf
+++ gitops-helm/variables.tf
@@ -1,11 +1,11 @@
-variable "host" {
-  type = string
-}
-
-variable "username" {
-  type = string
-}
-
-variable "password" {
-  type = string
-}
+#variable "host" {
+#  type = string
+#}
+#
+#variable "username" {
+#  type = string
+#}
+#
+#variable "password" {
+#  type = string
+#}
