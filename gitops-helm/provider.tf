a8981b861e9b8472477bff3b1743ca5c3c2aa1b3
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 16:51:48 -0400

    .

--- gitops-helm/provider.tf
+++ gitops-helm/provider.tf
@@ -1,6 +1,9 @@
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    #config_path = "~/.kube/config"
+  host     = "https://api.bosez123.qzzw.p1.openshiftapps.com"
+  username = "bolauder"
+  password = "Bolauder-password-123"    
   }
 }
 
