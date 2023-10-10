65b318cec5e2ad33a8325357073637b2060088af
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 16:42:48 -0400

    .

--- gitops-helm/provider.tf
+++ gitops-helm/provider.tf
@@ -11,7 +11,17 @@ terraform {
   }
 }
 
+#provider "kubernetes" {
+#    config_path = "~/.kube/config"
+#    config_context = "default/api-bosez123-qzzw-p1-openshiftapps-com:6443/bolauder"
+#}
+
 provider "kubernetes" {
-    config_path = "~/.kube/config"
-    config_context = "default/api-bosez123-qzzw-p1-openshiftapps-com:6443/bolauder"
+  host     = "https://api.bosez123.qzzw.p1.openshiftapps.com"
+  username = "bolauder"
+  password = "Bolauder-password-123"
+  insecure = true
+  #client_certificate     = "${file("~/.kube/client-cert.pem")}"
+  #client_key             = "${file("~/.kube/client-key.pem")}"
+  #cluster_ca_certificate = "${file("~/.kube/cluster-ca-cert.pem")}"
 }
