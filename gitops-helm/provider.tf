d1ef1cbae4526a68293b80b3d2f39f4c59cdf522
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-10 16:25:50 -0400

    .

--- gitops-helm/provider.tf
+++ gitops-helm/provider.tf
@@ -7,16 +7,6 @@ terraform {
   }
 }
 
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.11.0"
-    }
-  }
-}
-
-
 provider "kubernetes" {
     config_path = "~/.kube/config"
     config_context = "default/api-bosez123-qzzw-p1-openshiftapps-com:6443/bolauder"
