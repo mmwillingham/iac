c87b4de30547f2ba09fb6b6f2785933a53baacab
mmwillingham <martin.willingham@gmail.com>
2023-10-13 16:27:17 -0400

    .

--- get_token_shell/provider.tf
+++ get_token_shell/provider.tf
@@ -15,16 +15,16 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
 
 provider "helm" {
  kubernetes {
-   #host     = "https://api.bosez123.qzzw.p1.openshiftapps.com:6443"
-   #token = data.shell_script.token.output["ocp_token"]
-   #insecure = true
-   config_path = "~/.kube/config"
+   host     = "https://api.bosez123.qzzw.p1.openshiftapps.com:6443"
+   token = data.shell_script.token.output["ocp_token"]
+   insecure = true
+   #config_path = "~/.kube/config"
  }
 }
 
