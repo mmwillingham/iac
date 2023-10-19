9131789c2b486c31925d2c1c9fd21bfaecda94ec
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-19 11:13:06 -0400

    uncomment gitops and day2

--- day2/main.tf
+++ day2/main.tf
@@ -12,26 +12,26 @@ output "ocp_token" {
 }
 
 # Comment out everything below here to destroy resources
-# resource "helm_release" "gitops" {
-#   name        = "gitops"
-#   chart       = "gitops"
-#   repository  = "."
-#   namespace   = "helm-gitops"
-#   max_history = 3
-#   create_namespace = true
-#   wait             = true
-#   reset_values     = true
-# }
+resource "helm_release" "gitops" {
+  name        = "gitops"
+  chart       = "gitops"
+  repository  = "."
+  namespace   = "helm-gitops"
+  max_history = 3
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+}
 
 
 # Comment out everything below here to destroy resources
-# resource "helm_release" "app-of-apps" {
-#   name        = "app-of-apps"
-#   chart       = "app-of-apps"
-#   repository  = "."
-#   namespace   = "helm-app-of-apps"
-#   max_history = 3
-#   create_namespace = true
-#   wait             = true
-#   reset_values     = true
-# }
+resource "helm_release" "app-of-apps" {
+  name        = "app-of-apps"
+  chart       = "app-of-apps"
+  repository  = "."
+  namespace   = "helm-app-of-apps"
+  max_history = 3
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+}
