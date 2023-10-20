c87d3c96d987150f0babb043ac0160b9f06681fc
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-20 14:56:45 -0400

    delete

--- day2/main.tf
+++ day2/main.tf
@@ -24,28 +24,28 @@ output "ocp_token" {
 # }
 
 #Comment out everything below here to destroy resources
-resource "helm_release" "gitops" {
-  name        = "gitops-operator"
-  chart       = "gitops-operator"
-  repository  = "."
-  namespace   = "helm-gitops"
-  max_history = 3
-  create_namespace = true
-  wait             = true
-  reset_values     = true
-}
+# resource "helm_release" "gitops" {
+#   name        = "gitops-operator"
+#   chart       = "gitops-operator"
+#   repository  = "."
+#   namespace   = "helm-gitops"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
+# }
 
-#Comment out everything below here to destroy resources
-resource "helm_release" "gitops-operands" {
-  name        = "gitops-operands"
-  chart       = "gitops-operands"
-  repository  = "."
-  namespace   = "helm-gitops"
-  max_history = 3
-  create_namespace = true
-  wait             = true
-  reset_values     = true
-}
+# #Comment out everything below here to destroy resources
+# resource "helm_release" "gitops-operands" {
+#   name        = "gitops-operands"
+#   chart       = "gitops-operands"
+#   repository  = "."
+#   namespace   = "helm-gitops"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
+# }
 
 # Comment out everything below here to destroy resources
 # resource "helm_release" "app-of-apps" {
