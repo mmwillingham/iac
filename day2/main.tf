04e442dfb6dcff77ddf1c658d7760d76916fd285
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-10-20 14:50:07 -0400

    separated oerators and operands

--- day2/main.tf
+++ day2/main.tf
@@ -12,9 +12,21 @@ output "ocp_token" {
 }
 
 #Comment out everything below here to destroy resources
+# resource "helm_release" "gitops" {
+#   name        = "gitops"
+#   chart       = "gitops"
+#   repository  = "."
+#   namespace   = "helm-gitops"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
+# }
+
+#Comment out everything below here to destroy resources
 resource "helm_release" "gitops" {
-  name        = "gitops"
-  chart       = "gitops"
+  name        = "gitops-operator"
+  chart       = "gitops-operator"
   repository  = "."
   namespace   = "helm-gitops"
   max_history = 3
@@ -23,6 +35,17 @@ resource "helm_release" "gitops" {
   reset_values     = true
 }
 
+#Comment out everything below here to destroy resources
+# resource "helm_release" "gitops" {
+#   name        = "gitops-operands"
+#   chart       = "gitops-operands"
+#   repository  = "."
+#   namespace   = "helm-gitops"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
+# }
 
 # Comment out everything below here to destroy resources
 # resource "helm_release" "app-of-apps" {
