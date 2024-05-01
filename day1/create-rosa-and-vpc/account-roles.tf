36ac3e697ee5dc1134d27bf8c322f60c807d20f0
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-01 14:18:20 -0400

    Update account-roles.tf

--- day1/create-rosa-and-vpc/account-roles.tf
+++ day1/create-rosa-and-vpc/account-roles.tf
@@ -11,7 +11,8 @@ module "create_account_roles" {
 
   account_role_prefix    = local.cluster_name
   path                   = var.path
-  rosa_openshift_version = regex("^[0-9]+\\\\.[0-9]+", var.rosa_openshift_version)
+#  rosa_openshift_version = regex("^[0-9]+\\\\.[0-9]+", var.rosa_openshift_version)
+  rosa_openshift_version = var.rosa_openshift_version)
   account_role_policies  = data.rhcs_policies.all_policies.account_role_policies
   all_versions           = data.rhcs_versions.all
   operator_role_policies = data.rhcs_policies.all_policies.operator_role_policies
