0dcb524f8409ddacc6cdc1b0c69def93a918fba9
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-01 13:59:22 -0400

    Update variables.tf

--- day1/create-rosa-and-vpc/variables.tf
+++ day1/create-rosa-and-vpc/variables.tf
@@ -1,62 +1,46 @@
-variable "token" {
-  type      = string
-  sensitive = true
-}
-
-variable "operator_role_prefix" {
-  type = string
-}
-
-variable "url" {
+variable "rosa_openshift_version" {
   type        = string
-  description = "Provide OCM environment by setting a value to url"
-  default     = "https://api.openshift.com"
+  default     = "4.15.0"
+  description = "Desired version of OpenShift for the cluster, for example '4.15.0'. If version is greater than the currently running version, an upgrade will be scheduled."
 }
 
-variable "tags" {
-  description = "(optional) List of AWS resource tags to apply."
-  type        = map(string)
-  default = {
-    contact     = "xyz@company.com"
-    cost-center = "12345"
-    owner       = "productteam"
-    environment = "test"
-  }
+variable "account_role_policies" {
+  description = "account role policies details for account roles creation"
+  type = object({
+    sts_installer_permission_policy             = string
+    sts_support_permission_policy               = string
+    sts_instance_worker_permission_policy       = string
+    sts_instance_controlplane_permission_policy = string
+  })
+  default = null
 }
 
-variable "account_role_prefix" {
-  type = string
+variable "operator_role_policies" {
+  description = "operator role policies details for operator roles creation"
+  type = object({
+    openshift_cloud_credential_operator_cloud_credential_operator_iam_ro_creds_policy = string
+    openshift_cloud_network_config_controller_cloud_credentials_policy                = string
+    openshift_cluster_csi_drivers_ebs_cloud_credentials_policy                        = string
+    openshift_image_registry_installer_cloud_credentials_policy                       = string
+    openshift_ingress_operator_cloud_credentials_policy                               = string
+    openshift_machine_api_aws_cloud_credentials_policy                                = string
+  })
+  default = null
 }
 
-variable "ocm_environment" {
-  type    = string
-  default = "production"
-}
-
+# ROSA Cluster info
 variable "cluster_name" {
-  type    = string
-  default = "mmw-cluster"
+  default     = null
+  type        = string
+  description = "Provide the name of your ROSA cluster."
 }
 
-# variable "host_prefix" {
-#   type    = string
-#   default = "mmw_prefix"
-# }
-
-
-variable "cloud_region" {
-  type    = string
-  default = "us-east-2"
-}
-
-variable "availability_zones" {
-  type    = list(string)
-  default = ["us-east-2a"]
-}
-
-variable "openshift_version" {
-  type = string
-  default = ""
+variable "additional_tags" {
+  default = {
+    Terraform   = "true"
+  }
+  description = "Additional AWS resource tags"
+  type        = map(string)
 }
 
 variable "path" {
@@ -65,23 +49,33 @@ variable "path" {
   default     = null
 }
 
-variable "admin_username" {
-  type = string
+variable "machine_type" {
+  description = "The AWS instance type used for your default worker pool."
+  type        = string
+  default     = "m5.xlarge"
 }
 
-variable "admin_password" {
-  type = string
+variable "worker_node_replicas" {
+  default     = 2
+  description = "Number of worker nodes to provision. Single zone clusters need at least 2 nodes, multizone clusters need at least 3 nodes"
+  type        = number
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string  
+variable "autoscaling_enabled" {
+  description = "Enables autoscaling. This variable requires you to set a maximum and minimum replicas range using the 'max_replicas' and 'min_replicas' variables. If the autoscaling_enabled is 'true', you cannot configure the worker_node_replicas."
+  type        = string
+  default     = "false"
 }
 
-variable "private" {
-  type = string
-  default = "false"
+#VPC Info
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The value of the IP address block for machines or cluster nodes for the VPC."
+  default     = "10.0.0.0/16"
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
+#AWS Info
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
