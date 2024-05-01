8708c3b245274320fbcab0225bc581fea73e4f9a
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-01 15:18:27 -0400

    Update output.tf

--- day1/create-rosa-and-vpc/output.tf
+++ day1/create-rosa-and-vpc/output.tf
@@ -1,3 +1,33 @@
+output "cluster_id" {
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
+}
+
+output "oidc_thumbprint" {
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
+}
+
+output "oidc_endpoint_url" {
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
+}
+
+output domain {
+   value = rhcs_cluster_rosa_classic.rosa_sts_cluster.domain
+}
+
+output "cluster_admin_username" {
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials == null ? null : rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials.username
+}
+
+output "cluster_admin_password" {
+  value     = rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials == null ? null : rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials.password
+  sensitive = true
+}
+
+output api_url {
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.api_url
+}
+
+# OLD
 output "account_role_prefix" {
   value = module.create_account_roles.account_role_prefix
 }
