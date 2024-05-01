3b0c71be0379d622557d939eaad7374699dc4b75
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-01 13:57:26 -0400

    Update main.tf

--- day1/create-rosa-and-vpc/main.tf
+++ day1/create-rosa-and-vpc/main.tf
@@ -1,4 +1,19 @@
-# https://github.com/terraform-redhat/terraform-aws-rosa-sts/blob/main/examples/operator_roles_and_oidc/create_rosa_sts_operator_roles_and_oidc.tf
+#
+# Copyright (c) 2023 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 terraform {
   required_providers {
     aws = {
@@ -6,93 +21,84 @@ terraform {
       version = ">= 4.20.0"
     }
     rhcs = {
-      version = ">= 1.0.0"
+      version = ">= 1.5.0"
       source  = "terraform-redhat/rhcs"
     }
   }
 }
 
-provider "rhcs" {
-  token = var.token
-  url   = var.url
+# Export token using the RHCS_TOKEN environment variable
+provider "rhcs" {}
+
+provider "aws" {
+  region = var.aws_region
+  ignore_tags {
+    key_prefixes = ["kubernetes.io/"]
+  }
 }
 
+data "aws_availability_zones" "available" {}
 
-data "rhcs_rosa_operator_roles" "operator_roles" {
-  operator_role_prefix = var.operator_role_prefix
-  account_role_prefix = var.account_role_prefix
+locals {
+  # Extract availability zone names for the specified region, limit it to 1
+  region_azs = slice([for zone in data.aws_availability_zones.available.names : format("%s", zone)], 0, 1)
 }
 
-module operator_roles {
-    source = "terraform-redhat/rosa-sts/aws"
-    version = "0.0.5"
-    
-    create_account_roles = true    
-    create_operator_roles = true
-    create_oidc_provider = true
-
-    cluster_id = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
-    rh_oidc_provider_thumbprint = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
-    rh_oidc_provider_url = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
-    operator_roles_properties = data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles
-
-    #optional
-    tags                = {
-      contact     = "xyz@company.com"
-      cost-center = "12345"
-      owner       = "productteam"
-      environment = "test"
-    }
+resource "random_string" "random_name" {
+  length           = 6
+  special          = false
+  upper            = false
 }
 
-resource "time_sleep" "wait_for_roles" {
-  create_duration = "20s"
-  depends_on = [ module.create_account_roles ]
+locals {
+  path = coalesce(var.path, "/")
+  sts_roles = {
+    role_arn         = "arn:aws:iam::\${data.aws_caller_identity.current.account_id}:role\${local.path}\${local.cluster_name}-Installer-Role",
+    support_role_arn = "arn:aws:iam::\${data.aws_caller_identity.current.account_id}:role\${local.path}\${local.cluster_name}-Support-Role",
+    instance_iam_roles = {
+      master_role_arn = "arn:aws:iam::\${data.aws_caller_identity.current.account_id}:role\${local.path}\${local.cluster_name}-ControlPlane-Role",
+      worker_role_arn = "arn:aws:iam::\${data.aws_caller_identity.current.account_id}:role\${local.path}\${local.cluster_name}-Worker-Role"
+    },
+    operator_role_prefix = local.cluster_name,
+    oidc_config_id       = rhcs_rosa_oidc_config.oidc_config.id
+  }
+  worker_node_replicas = coalesce(var.worker_node_replicas, 2)
+  # If cluster_name is not null, use that, otherwise generate a random cluster name
+  cluster_name = coalesce(var.cluster_name, "rosa-\${random_string.random_name.result}")
+}
+
+data "aws_caller_identity" "current" {
 }
 
 resource "rhcs_cluster_rosa_classic" "rosa_sts_cluster" {
-  name               = var.cluster_name
-  cloud_region       = var.cloud_region
-  #host_prefix        = var.host_prefix
-  aws_account_id     = data.aws_caller_identity.current.account_id
-  availability_zones = var.availability_zones
+  name                 = local.cluster_name
+  cloud_region         = var.aws_region
+  multi_az             = false
+  aws_account_id       = data.aws_caller_identity.current.account_id
+  availability_zones   = ["us-east-1a"]
+  tags                 = var.additional_tags
+  version              = var.rosa_openshift_version
+  compute_machine_type = var.machine_type
+  replicas             = local.worker_node_replicas
+  autoscaling_enabled  = false
+  sts                  = local.sts_roles
   properties = {
     rosa_creator_arn = data.aws_caller_identity.current.arn
   }
-  sts = local.sts_roles  
-  destroy_timeout = 60  
-  depends_on = [time_sleep.wait_for_roles]
-  version = var.openshift_version
-  admin_credentials = {
-     password = var.admin_password
-     username = var.admin_username 
-  }  
+  machine_cidr     = var.vpc_cidr_block
+
+  lifecycle {
+    precondition {
+      condition     = can(regex("^[a-z][-a-z0-9]{0,13}[a-z0-9]\$", local.cluster_name))
+      error_message = "ROSA cluster name must be less than 16 characters, be lower case alphanumeric, with only hyphens."
+    }
+  }
+
+  depends_on = [time_sleep.wait_10_seconds]
 }
 
-resource "rhcs_cluster_wait" "rosa_cluster" {
+resource "rhcs_cluster_wait" "wait_for_cluster_build" {
   cluster = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
-  timeout = 60 
+  # timeout in minutes
+  timeout = 60
 }
-
-#module operator_roles {
-#    source = "terraform-redhat/rosa-sts/aws"
-#    version = "0.0.5"
-#
-#    create_operator_roles = true
-#    create_oidc_provider = true
-#
-#    cluster_id = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
-#    rh_oidc_provider_thumbprint = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
-#    rh_oidc_provider_url = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
-#    operator_roles_properties = data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles
-#
-#    #optional
-#    tags                = {
-#      contact     = "xyz@company.com"
-#      cost-center = "12345"
-#      owner       = "productteam"
-#      environment = "test"
-#    }
-#}
-
-
