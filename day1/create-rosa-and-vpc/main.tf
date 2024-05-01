d31e3ec5cd340b56244e505834eb67300d1a9e9a
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-01 12:22:27 -0400

    Update main.tf

--- day1/create-rosa-and-vpc/main.tf
+++ day1/create-rosa-and-vpc/main.tf
@@ -51,7 +51,97 @@ module "create_account_roles" {
   tags                = var.tags
 }
 
+module "rosa-sts" {
+  source  = "terraform-redhat/rosa-sts/aws"
+  version = "0.0.15"
+}
 
+provider "aws" {
+  region = var.cloud_region  
+}
+
+provider "rhcs" {
+  token = var.token
+  url   = var.url
+}
+
+module "create_account_roles"{
+   source = "terraform-redhat/rosa-sts/aws"
+   version = "0.0.5"
+
+   create_account_roles = true
+
+   account_role_prefix      = var.account_role_prefix
+   path                     = var.path
+   ocm_environment          = var.ocm_environment
+   rosa_openshift_version   = var.rosa_openshift_version
+   account_role_policies    = var.account_role_policies
+   all_versions             = var.all_versions
+   operator_role_policies   = var.operator_role_policies
+
+    #optional
+    tags                    = {
+      contact     = "xyz@company.com"
+      cost-center = "12345"
+      owner       = "productteam"
+      environment = "test"
+    }
+}
+
+resource "time_sleep" "wait_for_roles" {
+  create_duration = "20s"
+  depends_on = [ module.create_account_roles ]
+}
+
+resource "rhcs_cluster_rosa_classic" "rosa_sts_cluster" {
+  name               = var.cluster_name
+  cloud_region       = var.cloud_region
+  #host_prefix        = var.host_prefix
+  aws_account_id     = data.aws_caller_identity.current.account_id
+  availability_zones = var.availability_zones
+  properties = {
+    rosa_creator_arn = data.aws_caller_identity.current.arn
+  }
+  sts = local.sts_roles  
+  destroy_timeout = 60  
+  depends_on = [time_sleep.wait_for_roles]
+  version = var.openshift_version
+  admin_credentials = {
+     password = var.admin_password
+     username = var.admin_username 
+  }  
+}
+
+resource "rhcs_cluster_wait" "rosa_cluster" {
+  cluster = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
+  timeout = 60 
+}
+
+data "rhcs_rosa_operator_roles" "operator_roles" {
+  operator_role_prefix = var.operator_role_prefix
+  account_role_prefix = var.account_role_prefix
+}
+
+module operator_roles {
+    source = "terraform-redhat/rosa-sts/aws"
+    version = "0.0.5"
+
+    create_operator_roles = true
+    create_oidc_provider = true
+
+    cluster_id = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
+    rh_oidc_provider_thumbprint = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
+    rh_oidc_provider_url = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
+    operator_roles_properties = data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles
+
+    #optional
+    tags                = {
+      contact     = "xyz@company.com"
+      cost-center = "12345"
+      owner       = "productteam"
+      environment = "test"
+    }
+}
 
 ##################
 # OLD
